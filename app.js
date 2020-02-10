df25bf9ac6fe0bcd947b2bf3a97c927f28e2aa39
YOUHOBITS <60798283+YOUHOBITS@users.noreply.github.com>
2020-02-10 22:07:21 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,16 +1,1 @@
-<?php
-/*КЛЮЧИ*/
-define('SITE_KEY', '6LfH-aIUAAAAANTmsRDS2lZRIjjOxL4Q9q0fIFTl');
-define('SECRET_KEY', '6LfH-aIUAAAAAEWJIQJizklZldRxFeTGseZSbRxu');
-...
-...
-?>
-<script src="https://www.google.com/recaptcha/api.js?render=<?php echo SITE_KEY?>"></script>
-    <script>
-        grecaptcha.ready(function() {
-            grecaptcha.execute('<?php echo SITE_KEY;?>', {action: 'homepage'}).then(function(token) {
-                //console.log(token);
-                document.getElementById('g-recaptcha-response').value=token;
-            });
-        });
-  </script>
+
